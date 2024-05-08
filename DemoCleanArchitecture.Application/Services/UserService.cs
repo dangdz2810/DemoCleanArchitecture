fdf7302c6827f7d0bf3665bc36695386ed0e88f2
haidang <nonome2810@gmail.com>
2024-05-08 14:11:04 +0700

    hot-fix

--- DemoCleanArchitecture.Application/Services/UserService.cs
+++ DemoCleanArchitecture.Application/Services/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using DemoCleanArchitecture.Domain.Entities;
 using DemoCleanArchitecture.Domain.Interfaces.IServices;
 using DemoCleanArchitecture.Domain.Interfaces;
+using DemoCleanArchitecture.Application.Exceptions;
 
 namespace DemoCleanArchitecture.Application.Services
 {
@@ -43,6 +44,52 @@ namespace DemoCleanArchitecture.Application.Services
 
             // Thực hiện thêm mới user
             return await _userRepository.Create(registerViewModel);
+        }
+        public async Task DeleteExpiredRequests()
+        {
+            await _userRepository.DeleteExpiredRequests();
+        }
+
+        public async Task SendForgotPasswordEmail(ForgotPasswordRequest request)
+        {
+            var isUserExists = await _userRepository.GetByUsername(request.UserName);
+
+
+            if (isUserExists is null)
+            {
+                throw new DataNotFoundException("User is not existed");
+            }
+
+            var confirmationCode = GenerateRandomCode();
+
+            request.ExpirationTime = DateTime.UtcNow.AddMinutes(2);
+            request.Code = confirmationCode;
+
+            await _userRepository.AddRequest(request);
+            _userRepository.SendConfirmationEmail(isUserExists.Email, confirmationCode);
+        }
+
+        public async Task VerifyCodeResetPassword(ForgotPasswordRequest request)
+        {
+            var confirmCode = await _userRepository.GetByUserNameAndCode(request.UserName, request.Code);
+
+            if (confirmCode is null)
+            {
+                throw new DataNotFoundException("Confirm code is invalid");
+            }
+
+            if (confirmCode.ExpirationTime < DateTime.UtcNow)
+            {
+                await _userRepository.DeleteExpiredRequests();
+                throw new DataNotFoundException("The code has expired");
+            }
+
+        }
+        private string GenerateRandomCode()
+        {
+            Random random = new Random();
+            const string chars = "0123456789";
+            return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 }
